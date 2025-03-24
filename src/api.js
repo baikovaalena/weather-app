6bebc59fb47212c99098f1a029849d42b2bcc04f
AlenaBaikova <baikovaa19@gmail.com>
2025-03-24 21:41:59 +0500

    fix api function

--- src/api.js
+++ src/api.js
@@ -11,7 +11,7 @@ export async function getWeekWeather(cityName) {
 
 
 export async function getCurrentWeather(city) {
-    const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`)
     if (!response.ok) {
         throw new Error(response.statusText)
     }
